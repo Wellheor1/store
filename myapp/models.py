d07bc760db8c414f30292a00040ade790f7e650e
Well <Well.heor@yandex.ru>
2022-03-28 12:14:32 +0800

    API - get_*_list

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,7 @@
+import datetime
+
+import ins as ins
+import products.products
 from django.db import models
 
 
@@ -10,12 +14,37 @@ class Customer(models.Model):
     def __str__(self):
         return f'{self.first_name} {self.last_name} {self.patronymic}'
 
+    @staticmethod
+    def get_customer(params=None):
+        customers = Customer.objects.all()
+        data_serializer = []
+        for customer in customers:
+            data_serializer.append({
+                "id": customer.id,
+                "last_name": customer.last_name,
+                "first_name": customer.first_name,
+                "patronymic": customer.patronymic,
+                "address": customer.address
+            })
+        return data_serializer
+
 
 class Type(models.Model):
     title = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title
+
+    @staticmethod
+    def get_type(params=None):
+        types = Type.objects.all()
+        data_serializer = []
+        for type in types:
+            data_serializer.append({
+                "id": type.id,
+                "title": type.title,
+            })
+        return data_serializer
 
 
 class Manufacturer(models.Model):
@@ -24,6 +53,17 @@ class Manufacturer(models.Model):
     def __str__(self):
         return self.title
 
+    @staticmethod
+    def get_manufacturer(params=None):
+        manufacturers = Manufacturer.objects.all()
+        data_serializer = []
+        for manufacturer in manufacturers:
+            data_serializer.append({
+                "id": manufacturer.id,
+                "title": manufacturer.title,
+            })
+        return data_serializer
+
 
 class Model(models.Model):
     title = models.CharField(max_length=255)
@@ -31,6 +71,18 @@ class Model(models.Model):
 
     def __str__(self):
         return self.title
+
+    @staticmethod
+    def get_model(params=None):
+        models = Model.objects.all()
+        data_serializer = []
+        for model in models:
+            data_serializer.append({
+                "id": model.id,
+                "title": model.title,
+                "manufacturer_id": model.manufacturer_id.title
+            })
+        return data_serializer
 
 
 class Product(models.Model):
@@ -52,15 +104,13 @@ class Product(models.Model):
             data_serializer.append({
                 "id": product.id,
                 "title": product.title,
-                "type_id": product.type_id.id,
-                "model_id": product.model_id.id,
-                "manufacturer_id": product.manufacturer_id.id,
+                "type_id": product.type_id.title,
+                "model_id": product.model_id.title,
+                "manufacturer_id": product.manufacturer_id.title,
                 "description": product.description,
                 "price": product.price
             })
         return data_serializer
-
-
 
 
 class Order(models.Model):
@@ -69,4 +119,20 @@ class Order(models.Model):
     date_time = models.DateTimeField()
 
     def __str__(self):
-        return f'Номер заказа: {self.id} {Product.title}'
+        return f'Номер заказа: {self.id}'
+
+    @staticmethod
+    def get_order(params=None):
+        orders = Order.objects.all()
+        data_serializer = []
+        for order in orders:
+            data_serializer.append({
+                "id": order.id,
+                "customer_last_name": order.customer_id.last_name,
+                "customer_first_name": order.customer_id.first_name,
+                "customer_patronymic": order.customer_id.patronymic,
+                "products": order.products,
+                "date_time": order.date_time.strftime('%H:%M %Y.%m.%d')
+            })
+        print(data_serializer)
+        return data_serializer
--- myapp/views.py
+++ myapp/views.py
@@ -4,12 +4,36 @@ from .models import Product, Customer, Type, Model, Manufacturer, Order
 
 
 @csrf_exempt
-def get_product_list(request):
-    data_serializer = Product.get_product()
-
+def get_customer_list(request):
+    data_serializer = Customer.get_customer()
     return JsonResponse({"data": data_serializer})
 
 
+@csrf_exempt
+def get_type_list(request):
+    data_serializer = Type.get_type()
+    return JsonResponse({"data": data_serializer})
 
 
+@csrf_exempt
+def get_manufacturer_list(request):
+    data_serializer = Manufacturer.get_manufacturer()
+    return JsonResponse({"data": data_serializer})
 
+
+@csrf_exempt
+def get_model_list(request):
+    data_serializer = Model.get_model()
+    return JsonResponse({"data": data_serializer})
+
+
+@csrf_exempt
+def get_product_list(request):
+    data_serializer = Product.get_product()
+    return JsonResponse({"data": data_serializer})
+
+
+@csrf_exempt
+def get_order_list(request):
+    data_serializer = Order.get_order()
+    return JsonResponse({"data": data_serializer})
